4a4a149189823e1b47591eb5a952078a8fed8771
Etienne Cointet <etienne@cointet.com>
2021-06-26 01:50:23 +0200

    new sms-server

---
+++ sms/sms-server/numbers.ts
@@ -0,0 +1,27 @@
+import {Controller,Get,Post,Res,HttpStatus} from '@nestjs/common';
+import {Response} from 'express';
+import {AppService} from './app.service';
+const plivo = require('plivo');
+
+@Controller()
+export class AppController {
+    constructor(private readonly appService: AppService) {}
+
+    @Get('/outbound')
+    sendSMS() {
+        const client = new plivo.Client('MAZTYZYTA3YMNJYTFMYW','OGI5OWQ0ZTc2MzY2Y2RkYjBjMjMzYzJkYjIzZDVi');
+        client.messages.create(
+                '+17253737999',
+                '+33633807171',
+                'Hello, from Nest!',
+            )
+            .then(
+                function(response) {
+                    console.log(response);
+                },
+                function(err) {
+                    console.error(err);
+                },
+            );
+    }
+}
